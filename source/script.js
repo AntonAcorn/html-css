186b47d2e2979a8a3bb3e3654de576cf5e84ba06
Anton Shustikov <ShustikovAnimation@gmail.com>
2024-06-20 07:45:56 -0400

    #Refactor range

--- source/script.js
+++ source/script.js
@@ -1,11 +1,10 @@
 const slider = document.getElementById('slider');
 const sliderValue = document.getElementById('slider-value');
+slider.style.background = `linear-gradient(to right, #d5d5d5 20px, #db4d0f 20px)`;
+
 slider.addEventListener('input', function () {
     sliderValue.textContent = this.value;
 
     const value = (this.value - this.min) / (this.max - this.min) * 100;
     this.style.background = `linear-gradient(to right, #d5d5d5 35px ${value}%, #db4d0f ${value}%)`;
 });
-
-const value = (slider.value - slider.min) / (slider.max - slider.min) * 100;
-slider.style.background = `linear-gradient(to right, #d5d5d5 20px ${value}%, #db4d0f ${value}%)`;
