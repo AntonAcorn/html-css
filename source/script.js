1009b5e5a3b1cdfe791abdef412cb7b8863bba82
Anton Shustikov <ShustikovAnimation@gmail.com>
2024-06-21 06:32:21 -0400

    #File input

--- source/script.js
+++ source/script.js
@@ -3,8 +3,7 @@ const sliderValue = document.getElementById('slider-value');
 slider.style.background = `linear-gradient(to right, #d5d5d5 20px, #db4d0f 20px)`;
 
 slider.addEventListener('input', function () {
-    sliderValue.textContent = this.value;
-
     const value = (this.value - this.min) / (this.max - this.min) * 100;
+    sliderValue.textContent = this.value;
     this.style.background = `linear-gradient(to right, #d5d5d5 35px ${value}%, #db4d0f ${value}%)`;
 });
